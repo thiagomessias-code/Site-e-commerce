7566a832a38526a5839d4c8bb9b3cd990dc65b44
Thiago Messias <thiagomessiasdevbr@gmail.com>
2021-06-19 16:28:40 -0300

    Inclusão modificações second part.

--- js/main.js
+++ js/main.js
@@ -81,18 +81,6 @@
   });
 
   /* 
-     VIDEO POP-UP
-     ========================================================================== */
-  $('.video-popup').magnificPopup({
-    disableOn: 700,
-    type: 'iframe',
-    mainClass: 'mfp-fade',
-    removalDelay: 160,
-    preloader: false,
-    fixedContentPos: false,
-  });
-
-  /* 
      Back Top Link
      ========================================================================== */
   var offset = 200;
