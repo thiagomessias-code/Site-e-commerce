fe790da91360643db1504861c709283a65af36a9
Thiago Messias <thiagomessiasdevbr@gmail.com>
2021-06-30 19:21:54 -0300

    form-teste

---
+++ js/email.php
@@ -0,0 +1,11 @@
+<?php
+if(isset($_POST(['email']) && !empty($_POST(['email'])){
+
+$nome = addslashes($_POST(['name']))
+$email = addslashes($_POST(['email']))
+$mensage = addslashes($_POST(['message']))
+
+}
+
+
+?>
